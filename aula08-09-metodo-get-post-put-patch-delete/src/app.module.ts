844668f332da72320e06ca0b420008b1ad850c78
Alerrandro Matos <ferreiramattos638@gmail.com>
2026-09-21 16:54:38 -0300

    feat: registra ConvidadosService no módulo principal

--- aula08-09-metodo-get-post-put-patch-delete/src/app.module.ts
+++ aula08-09-metodo-get-post-put-patch-delete/src/app.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ConvidadosController } from './convidados.controller.js';
+import { ConvidadosService } from './convidados.service.js';
 
 // export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [],
   controllers: [AppController, ConvidadosController],
-  providers: [AppService],
+  providers: [AppService, ConvidadosService],
 })
 export class AppModule {}
